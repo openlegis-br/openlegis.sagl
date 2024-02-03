3aaee4636717cd7ae79f875e55b188d721a57311
OpenLegis <contato@openlegis.com.br>
2024-02-02 23:52:45 -0300

    Update arquivos_expediente.py

--- openlegis/sagl/Extensions/arquivos_expediente.py
+++ openlegis/sagl/Extensions/arquivos_expediente.py
@@ -6,7 +6,9 @@ def baixar_pdf(context):
     cod_sessao_plen = context.REQUEST['cod_sessao_plen']
     for sessao in context.zsql.sessao_plenaria_obter_zsql(cod_sessao_plen=cod_sessao_plen, ind_excluido=0):
         for tipo_sessao in context.zsql.tipo_sessao_plenaria_obter_zsql(tip_sessao=sessao.tip_sessao):
-                zipname =  str('materias_lidas-') + str(sessao.num_sessao_plen) + str('ªReunião_') + str(tipo_sessao.nom_sessao) + str('.zip')         
+                nome_sessao = str(tipo_sessao.nom_sessao).decode("utf-8")
+                zipname = u'materias_lidas-%sa_Reuniao%s.zip' % (sessao.num_sessao_plen, nome_sessao)
+                #zipname =  str('materias_lidas-') + str(sessao.num_sessao_plen) + str('ªReunião_') + str(tipo_sessao.nom_sessao) + str('.zip')         
     foldername =  'proposicoes'
     dirpath = os.path.join('/tmp/', foldername)
     if not os.path.exists(dirpath):

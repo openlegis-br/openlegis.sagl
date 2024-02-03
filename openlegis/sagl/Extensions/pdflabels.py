52a8e260f5767b51988ec4963059facdfd57535e
OpenLegis <contato@openlegis.com.br>
2024-02-03 18:36:44 -0300

    Update pdflabels.py

--- openlegis/sagl/Extensions/pdflabels.py
+++ openlegis/sagl/Extensions/pdflabels.py
@@ -121,7 +121,7 @@ class LabelGenerator:
         self.horizSpacing = spec.get('horizontalSpacing', 0) * self.un
         
         self.font = "Helvetica"
-        self.size = 10
+        self.size = 8
 	self.leadingFactor = 1.2
         
         try:

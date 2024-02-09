3793a005079bddb333fd2909e9e98cfea5b667ac
OpenLegis <contato@openlegis.com.br>
2024-02-09 02:53:52 -0300

    Update assinar_lote_pdf.py

--- openlegis/sagl/skins/cadastros/proposicao/assinar_lote_pdf.py
+++ openlegis/sagl/skins/cadastros/proposicao/assinar_lote_pdf.py
@@ -13,7 +13,6 @@ REQUEST = context.REQUEST
 
 lista = []
 for proposicao in context.zsql.proposicao_obter_zsql(ind_excluido=0, ind_enviado='0', ind_devolvido='0', col_username=REQUEST['AUTHENTICATED_USER'].getUserName()):
-#for proposicao in context.zsql.proposicao_obter_zsql(ind_excluido=0, ind_enviado='0', ind_devolvido='0', col_username='abatenio'):
     if proposicao.des_tipo_proposicao=='Requerimento' or proposicao.des_tipo_proposicao=='Indicação' or proposicao.des_tipo_proposicao=='Moção':
        id_documento = str(proposicao.cod_proposicao)+'.pdf'
        id_documento_assinado = str(proposicao.cod_proposicao)+'_signed.pdf'

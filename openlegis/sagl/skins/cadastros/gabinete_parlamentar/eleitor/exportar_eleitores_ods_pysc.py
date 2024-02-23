aa091753dba516542774fd0f722161e5fd814bbd
OpenLegis <contato@openlegis.com.br>
2024-02-23 20:53:56 -0300

    Update exportar_eleitores_ods_pysc.py

--- openlegis/sagl/skins/cadastros/gabinete_parlamentar/eleitor/exportar_eleitores_ods_pysc.py
+++ openlegis/sagl/skins/cadastros/gabinete_parlamentar/eleitor/exportar_eleitores_ods_pysc.py
@@ -4,7 +4,7 @@
 ##bind namespace=
 ##bind script=script
 ##bind subpath=traverse_subpath
-##parameters= cod_parlamentar_corrente, txt_nom_eleitor, txt_dat_atendimento, txt_dat_atendimento2, txt_dia_aniversario, lst_mes_aniversario, rad_sex_eleitor, txt_des_estado_civil, rad_filhos, txt_des_profissao, txt_des_local_trabalho, txt_end_residencial, txt_nom_bairro, txt_num_cep, txt_nom_localidade, lst_txt_classe
+##parameters= cod_parlamentar_corrente, txt_nom_eleitor, txt_dat_atendimento, txt_dat_atendimento2, txt_dia_aniversario, lst_mes_aniversario, rad_sex_eleitor, txt_des_estado_civil, rad_filhos, txt_des_profissao, txt_des_local_trabalho, txt_end_residencial, txt_nom_bairro, txt_num_cep, txt_nom_localidade, lst_txt_classe, lst_assessor
 ##title=
 ##
 
@@ -31,7 +31,8 @@ for item in context.zsql.gabinete_eleitor_pesquisar_zsql(
                                                nom_bairro=REQUEST['txt_nom_bairro'],
                                                num_cep=REQUEST['txt_num_cep'],
                                                nom_localidade=REQUEST['txt_nom_localidade'],
-                                               txt_classe=REQUEST['lst_txt_classe']
+                                               txt_classe=REQUEST['lst_txt_classe'],
+                                               cod_assessor=REQUEST['lst_assessor']
                                                ):
 
     eleitor = {}
@@ -57,4 +58,3 @@ for item in context.zsql.gabinete_eleitor_pesquisar_zsql(
 
 st = getToolByName(context, 'portal_sagl')
 return st.eleitores_exportar(eleitores)
-

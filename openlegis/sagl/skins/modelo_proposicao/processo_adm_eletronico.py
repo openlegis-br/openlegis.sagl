b8520117cff61bac1455bf9a14645a3511fcecba
OpenLegis <contato@openlegis.com.br>
2024-02-26 18:43:48 -0300

    Update processo_adm_eletronico.py

--- openlegis/sagl/skins/modelo_proposicao/processo_adm_eletronico.py
+++ openlegis/sagl/skins/modelo_proposicao/processo_adm_eletronico.py
@@ -4,11 +4,19 @@
 ##bind namespace=
 ##bind script=script
 ##bind subpath=traverse_subpath
-##parameters=cod_documento
+##parameters=
 ##title=
 ##
 from Products.CMFCore.utils import getToolByName
+from AccessControl import getSecurityManager
 
 st = getToolByName(context, 'portal_sagl')
+
+anon = getSecurityManager().getUser()
+if (str(anon) == 'Anonymous User'):
+   for documento in context.zsql.protocolo_pesquisa_publica_zsql(chave_acesso=context.REQUEST['token']):
+       cod_documento = documento.cod_documento
+else:
+   cod_documento = context.REQUEST['cod_documento']
 
 return st.processo_adm_gerar_pdf(cod_documento)

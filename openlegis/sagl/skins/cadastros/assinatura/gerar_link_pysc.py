c534af1b1bdb5debcb8fa128b01cb25f440355f6
OpenLegis <contato@openlegis.com.br>
2024-04-07 16:20:16 -0300

    Update gerar_link_pysc.py

--- openlegis/sagl/skins/cadastros/assinatura/gerar_link_pysc.py
+++ openlegis/sagl/skins/cadastros/assinatura/gerar_link_pysc.py
@@ -4,16 +4,23 @@
 ##bind namespace=
 ##bind script=script
 ##bind subpath=traverse_subpath
-##parameters=tip_documento,codigo
+##parameters=tip_documento, codigo, anexo
 ##title=
 ##
 
 for item in context.zsql.assinatura_storage_obter_zsql(tip_documento=tip_documento):
     location = item.pdf_location
-    pdf_signed = str(location) + str(codigo) + str(item.pdf_signed)
-    pdf_file = str(location) + str(codigo) + str(item.pdf_file)
-    nom_arquivo = str(codigo) + str(item.pdf_file)
-    nom_arquivo_assinado = str(codigo) + str(item.pdf_signed)
+    if anexo != None and anexo != '':
+       codigo = str(codigo) + '_anexo_' + str(anexo)
+       pdf_signed = str(location) + str(codigo) + str(item.pdf_signed)
+       pdf_file = str(location) + str(codigo) + str(item.pdf_file)
+       nom_arquivo = str(codigo) + str(item.pdf_file)
+       nom_arquivo_assinado = str(codigo) + str(item.pdf_signed)
+    else:
+       pdf_signed = str(location) + str(codigo) + str(item.pdf_signed)
+       pdf_file = str(location) + str(codigo) + str(item.pdf_file)
+       nom_arquivo = str(codigo) + str(item.pdf_file)
+       nom_arquivo_assinado = str(codigo) + str(item.pdf_signed)
 
 try:
    arquivo = context.restrictedTraverse(pdf_signed)

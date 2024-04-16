a9f2bf43897696b887160d2f6dc904809b961453
OpenLegis <contato@openlegis.com.br>
2024-04-16 11:42:10 -0300

    Update vereadores_atuais_json.py

--- openlegis/sagl/skins/consultas/parlamentar/vereadores_atuais_json.py
+++ openlegis/sagl/skins/consultas/parlamentar/vereadores_atuais_json.py
@@ -8,6 +8,7 @@
 ##title=
 ##
 import simplejson as json
+from xml.sax.saxutils import escape
 
 context.REQUEST.RESPONSE.setHeader("Access-Control-Allow-Origin", "*")
 request=context.REQUEST
@@ -29,15 +30,18 @@ for item in context.zsql.autores_obter_zsql(txt_dat_apresentacao=data_atual):
        dic['foto'] = request.SERVER_URL + '/sapl_documentos/parlamentar/fotos/' + foto
     else:
        dic['foto'] = request.SERVER_URL + '/imagens/avatar.png'   
-    dic['link'] = request.SERVER_URL + '/consultas/parlamentar/parlamentar_mostrar_proc?cod_parlamentar=' + item.cod_parlamentar + '%26iframe=1'
+    dic['link'] = request.SERVER_URL + '/consultas/parlamentar/parlamentar_mostrar_proc?cod_parlamentar=' + item.cod_parlamentar + '&iframe=0'
     dic['partido'] = ''
     for filiacao in context.zsql.parlamentar_data_filiacao_obter_zsql(num_legislatura=num_legislatura, cod_parlamentar=item.cod_parlamentar):    
         if filiacao.dat_filiacao != '0' and filiacao.dat_filiacao != None:
             for partido in context.zsql.parlamentar_partido_obter_zsql(dat_filiacao=filiacao.dat_filiacao, cod_parlamentar=item.cod_parlamentar):
                 dic['partido'] = partido.sgl_partido
     for parlamentar in context.zsql.parlamentar_obter_zsql(cod_parlamentar=item.cod_parlamentar):
-       dic['biografia'] = parlamentar.txt_biografia
-       dic['cod_parlamentar'] = parlamentar.cod_parlamentar              
+	    if parlamentar.txt_biografia != None:
+	       dic['biografia'] = escape(parlamentar.txt_biografia)
+	    else:
+	       dic['biografia'] = ''
+        dic['cod_parlamentar'] = parlamentar.cod_parlamentar              
     lista_exercicio.append(dic)
 
 lista_exercicio.sort(key=lambda dic: dic['nom_completo'])

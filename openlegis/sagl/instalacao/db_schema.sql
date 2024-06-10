d25001f88888f0e1092e78f417d52e9bd77a8720
OpenLegis <contato@openlegis.com.br>
2024-06-10 14:27:22 -0300

    Update db_schema.sql

--- openlegis/sagl/instalacao/db_schema.sql
+++ openlegis/sagl/instalacao/db_schema.sql
@@ -9,7 +9,7 @@ SET time_zone = "+00:00";
 /*!40101 SET NAMES utf8mb4 */;
 
 --
--- Banco de dados: `cmuberlandia_openlegis`
+-- Banco de dados: `openlegis`
 --
 
 -- --------------------------------------------------------

eb2ee19c095c7624ba623f35dfdaf421caa7d6eb
OpenLegis <contato@openlegis.com.br>
2025-02-04 19:17:01 -0300

    resultados carimbo

--- openlegis/sagl/skins/modelo_proposicao/requerimento_aprovar.py
+++ openlegis/sagl/skins/modelo_proposicao/requerimento_aprovar.py
@@ -11,5 +11,6 @@ from Products.CMFCore.utils import getToolByName
 
 st = getToolByName(context, 'portal_sagl')
 
-if nom_resultado == 'Aprovado' or nom_resultado == 'Rejeitado' or nom_resultado == 'Pedido de Retirada' or nom_resultado == 'Lido em Plenário':
+if nom_resultado == 'Deferido' or nom_resultado == 'Indeferido' or nom_resultado == 'Cancelado' or nom_resultado == 'Despachada' or nom_resultado == 'Aprovado(a)' or nom_resultado == 'Aprovado' or nom_resultado == 'Rejeitado' or nom_resultado == 'Rejeitado(a)' or nom_resultado == 'Retirado' or nom_resultado == 'Pedido de Retirada' or nom_resultado == 'Lido em Plenário' or nom_resultado == 'Não votada - falta de quorum' or nom_resultado == 'Excluído da pauta':
+
    return st.requerimento_aprovar(cod_sessao_plen, nom_resultado, cod_materia)

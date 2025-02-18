33237706b73e650df07cd2cbba2e3456dc6bd941
OpenLegis <contato@openlegis.com.br>
2025-02-17 21:33:10 -0300

    permissao autor peticao

--- openlegis/sagl/skins/pysc/autoria_materia_salvar_pysc.py
+++ openlegis/sagl/skins/pysc/autoria_materia_salvar_pysc.py
@@ -11,8 +11,11 @@ cod_materia = str(materia)
 
 lista = autores
 
-for item in lista:
-  context.zsql.autoria_incluir_zsql(cod_autor=item, cod_materia=cod_materia, ind_primeiro_autor=0)
+for i, item in enumerate(lista):
+    if i == 0:
+       context.zsql.autoria_incluir_zsql(cod_autor=item, cod_materia=cod_materia, ind_primeiro_autor=1)
+    else:
+       context.zsql.autoria_incluir_zsql(cod_autor=item, cod_materia=cod_materia, ind_primeiro_autor=0)
 
 return 1
 

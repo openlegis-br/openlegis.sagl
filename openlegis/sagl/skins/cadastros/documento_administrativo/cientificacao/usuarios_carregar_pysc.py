f93c2722376fa5dfefac9d439d6b836c595c4e7b
OpenLegis <contato@openlegis.com.br>
2025-05-19 13:17:43 -0300

    Update usuarios_carregar_pysc.py

--- openlegis/sagl/skins/cadastros/documento_administrativo/cientificacao/usuarios_carregar_pysc.py
+++ openlegis/sagl/skins/cadastros/documento_administrativo/cientificacao/usuarios_carregar_pysc.py
@@ -4,38 +4,35 @@
 ##bind namespace=
 ##bind script=script
 ##bind subpath=traverse_subpath
-##parameters= svalue, cod_documento
+##parameters=svalue, cod_documento
 ##title=
 ##
 import json
 
-context.REQUEST.RESPONSE.setHeader("Access-Control-Allow-Origin", "*")
+response = context.REQUEST.RESPONSE
+response.setHeader("Access-Control-Allow-Origin", "*")
 
-if svalue != '':
-   lst_usuarios = []
-   for usuario in context.zsql.usuario_unid_tram_obter_zsql(cod_unid_tramitacao = svalue):
-       nom_cargo = ''
-       if usuario.nom_cargo != '':
-          nom_cargo = ' (' + usuario.nom_cargo + ')'   
-       dic = {
-           'id': usuario.cod_usuario,
-           'name': usuario.nom_completo + nom_cargo,
-       }
-       if usuario.ind_leg != '0' or usuario.ind_adm != '0':
-          lst_usuarios.append(dic)
-   lst_usuarios.sort(key=lambda dic: dic['name'])
-   return json.dumps(lst_usuarios)
+def formatar_usuario(usuario):
+    """Formata o dicionário do usuário com nome e cargo."""
+    nome = usuario.nom_completo
+    cargo = usuario.nom_cargo or ''
+    if cargo:
+        nome += f" ({cargo})"
+    return {
+        'id': usuario.cod_usuario,
+        'name': nome,
+    }
 
+lst_usuarios = []
+
+if svalue:
+    usuarios = context.zsql.usuario_unid_tram_obter_zsql(cod_unid_tramitacao=svalue)
+    for usuario in usuarios:
+        if usuario.ind_leg != '0' or usuario.ind_adm != '0':
+            lst_usuarios.append(formatar_usuario(usuario))
+    lst_usuarios.sort(key=lambda u: u['name'])
 else:
-   lst_usuarios = []
-   for usuario in context.zsql.usuario_obter_zsql(ind_excluido=0, ind_ativo=1):
-       nom_cargo = ''
-       if usuario.nom_cargo != '':
-          nom_cargo = ' (' + usuario.nom_cargo + ')'
-       dic = {
-           'id': usuario.cod_usuario,
-           'name': usuario.nom_completo + nom_cargo,
-       }
-       lst_usuarios.append(dic)
-   return json.dumps(lst_usuarios)
- 
+    usuarios = context.zsql.usuario_obter_zsql(ind_excluido=0, ind_ativo=1)
+    lst_usuarios = [formatar_usuario(u) for u in usuarios]
+
+return json.dumps(lst_usuarios)

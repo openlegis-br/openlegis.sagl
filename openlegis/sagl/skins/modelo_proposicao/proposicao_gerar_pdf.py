48f996d7c79d805a30934e02916661f22d30a7c1
OpenLegis <contato@openlegis.com.br>
2025-07-10 14:31:16 -0300

    Update proposicao_gerar_pdf.py

--- openlegis/sagl/skins/modelo_proposicao/proposicao_gerar_pdf.py
+++ openlegis/sagl/skins/modelo_proposicao/proposicao_gerar_pdf.py
@@ -6,9 +6,18 @@
 ##bind subpath=traverse_subpath
 ##parameters=cod_proposicao
 ##title=
-##
+import json
 from Products.CMFCore.utils import getToolByName
 
+REQUEST = container.REQUEST
+RESPONSE = REQUEST.RESPONSE
 st = getToolByName(context, 'portal_sagl')
 
-return st.proposicao_gerar_pdf(cod_proposicao)
+try:
+    st.proposicao_gerar_pdf(cod_proposicao)
+    RESPONSE.setHeader('Content-Type', 'application/json; charset=utf-8')
+    return json.dumps({"success": True, "message": "PDF gerado com sucesso!"})
+except Exception as e:
+    RESPONSE.setStatus(500)
+    RESPONSE.setHeader('Content-Type', 'application/json; charset=utf-8')
+    return json.dumps({"success": False, "message": f"Erro ao gerar PDF: {e}"})

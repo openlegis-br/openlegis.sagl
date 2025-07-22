7d6d9c89c9db0add5b44db115cdc393293408a3b
OpenLegis <contato@openlegis.com.br>
2025-07-21 23:44:29 -0300

    Update tasks.py

--- tasks.py
+++ tasks.py
@@ -4,7 +4,7 @@ from tasks_folder.proposicao_autuar import proposicao_autuar_task
 from tasks_folder.peticao_autuar import peticao_autuar_task
 from tasks_folder.margem_inferior import margem_inferior_task
 from tasks_folder.adicionar_carimbo import adicionar_carimbo_task
-from tasks_folder.assinar_proposicao import assinar_proposicao_task
+#from tasks_folder.assinar_proposicao import assinar_proposicao_task
 from tasks_folder.indexar_arquivo import indexar_arquivo_task
 
 celery = Celery('tasks', config_source='celeryconfig')

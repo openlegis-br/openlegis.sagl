52d23393ea07ef690b123e5cba56e7f2052004f0
OpenLegis <contato@openlegis.com.br>
2025-08-04 09:51:42 -0300

    Update envia_tramitacao_autor_pysc.py

--- openlegis/sagl/skins/pysc/envia_tramitacao_autor_pysc.py
+++ openlegis/sagl/skins/pysc/envia_tramitacao_autor_pysc.py
@@ -6,88 +6,89 @@
 ##bind subpath=traverse_subpath
 ##parameters=cod_materia
 ##title=
-
 from xml.sax.saxutils import escape
+from Products.PythonScripts.standard import url_unquote
 from email.mime.text import MIMEText
-from DateTime import DateTime
-import logging
+request=context.REQUEST
+response=request.RESPONSE
 
-request = context.REQUEST
 mailhost = context.MailHost
 
-# Dados da casa legislativa
-props = dict(context.sapl_documentos.props_sagl.propertyItems())
-email_casa = props.get('end_email_casa', '').strip()
-casa_legislativa = props.get('nom_casa', 'Câmara Municipal')
+data_registro=DateTime(datefmt='international').strftime('%d/%m/%Y às %H:%M')
 
-if not email_casa or '@' not in email_casa:
-    logging.warning('Email da casa legislativa (remetente) não configurado corretamente.')
-    return
-
-data_registro = DateTime(datefmt='international').strftime('%d/%m/%Y às %H:%M')
+casa={}
+aux=context.sapl_documentos.props_sagl.propertyItems()
+for item in aux:
+  casa[item[0]] = item[1]
+email_casa = casa['end_email_casa']
+casa_legislativa = casa['nom_casa']
 
 for materia in context.zsql.materia_obter_zsql(cod_materia=cod_materia):
-    ementa = escape(materia.txt_ementa or '')
-    projeto = f"{materia.des_tipo_materia} {materia.num_ident_basica}/{materia.ano_ident_basica}"
+ ementa = materia.txt_ementa
+ projeto = str(materia.des_tipo_materia)+" "+str(materia.num_ident_basica)+"/"+str(materia.ano_ident_basica)
 
-    # Coleta autores únicos
-    lista_autor = []
-    lista_codigo = set()
-    for autor in context.zsql.autoria_obter_zsql(cod_materia=materia.cod_materia):
-        cod_autor = int(autor.cod_autor)
-        nome_autor = autor.nom_autor_join
-        if cod_autor not in lista_codigo:
-            lista_codigo.add(cod_autor)
-            lista_autor.append(nome_autor)
+ nom_autor = ""
+ autorias = context.zsql.autoria_obter_zsql(cod_materia=materia.cod_materia)
+ fields = list(autorias.data_dictionary().keys())
+ lista_autor = []
+ lista_codigo = []
+ for autor in autorias:
+   for field in fields:
+     cod_autor = int(autor['cod_autor'])
+     nome_autor = autor['nom_autor_join']
+   lista_codigo.append(cod_autor)
+   lista_autor.append(nome_autor)
+ nom_autor = ', '.join(['%s' % (value) for (value) in lista_autor])
 
-    nom_autor = ', '.join(lista_autor)
+ data = ''
+ status = ''
+ texto_acao = ''
+ for tramitacao in context.zsql.tramitacao_obter_zsql(cod_materia=cod_materia, ind_ult_tramitacao=1):
+   data = tramitacao.dat_tramitacao
+   status = tramitacao.des_status
+   if tramitacao.txt_tramitacao != None:
+     texto_acao = escape(tramitacao.txt_tramitacao)
+   else:
+     texto_acao = " "
 
-    # Informações da última tramitação
-    data = ''
-    status = ''
-    texto_acao = ''
-    for tramitacao in context.zsql.tramitacao_obter_zsql(cod_materia=cod_materia, ind_ult_tramitacao=1):
-        data = tramitacao.dat_tramitacao.strftime('%d/%m/%Y')
-        status = tramitacao.des_status or ''
-        texto_acao = escape(tramitacao.txt_tramitacao or '')
+remetente = email_casa
 
-    # Link da matéria
-    linkMat = f"{request.SERVER_URL}/consultas/materia/materia_mostrar_proc?cod_materia={cod_materia}"
+linkMat = request.SERVER_URL+"/consultas/materia/materia_mostrar_proc?cod_materia=" + str(cod_materia)
 
-    # Coleta e-mails válidos dos autores
-    destinatarios = set()
-    for cod_autor in lista_codigo:
-        for dest in context.zsql.autor_obter_zsql(cod_autor=cod_autor):
-            if dest.end_email and '@' in dest.end_email:
-                destinatarios.add(dest.end_email.strip())
+destinatarios=[]
+for item in lista_codigo:
+ for destinatario in context.zsql.autor_obter_zsql(cod_autor=item):
+  dic={}
+  dic['end_email']=destinatario.end_email
+  if dic['end_email'] != None:
+   destinatarios.append(dic)
 
-    # Monta e envia os e-mails
-    for email_dest in destinatarios:
-        assunto = f"{projeto} - Aviso de tramitação em {data_registro}"
-        html = f"""\
-        <html>
-          <head></head>
-          <body>
-            <p>A seguinte matéria de sua autoria sofreu tramitação registrada em {data_registro}:</p>
-            <p>
-              <a href="{linkMat}" target="_blank">{projeto}</a><br>
-              {ementa}<br>
-              Autoria: {nom_autor}<br>
-              Data da Ação: {data}<br>
-              Status: {status}
-            </p>
-            <p>
-              <strong>{casa_legislativa}</strong><br>
-              Processo Digital
-            </p>
-          </body>
-        </html>
-        """
+for dic in destinatarios:
+   html = """\
+   <html>
+     <head></head>
+     <body>
+       <p>A seguinte matéria de sua autoria sofreu tramitação registrada em {data_registro}:</p>
+       <p><a href="{linkMat}" target="blank">{projeto}</a><br>
+          {ementa}<br>
+          Autoria: {nom_autor}<br>    
+          Data da Ação: {data}<br>
+          Status: {status}
+       </p>
+       <p>
+          <strong>{casa_legislativa}</strong><br>
+          Processo Digital
+       </p>
+     </body>
+   </html>
+   """.format(data_registro=data_registro, projeto=projeto, linkMat=linkMat, ementa=ementa, nom_autor=nom_autor, data=data, status=status, casa_legislativa=casa_legislativa)
 
-        try:
-            # Verificação final antes do envio
-            if email_dest and email_casa and assunto and html:
-                mensagem = MIMEText(html, 'html', 'utf-8')
-                mailhost.send(mensagem, mTo=email_dest, mFrom=email_casa, subject=assunto)
-        except Exception as e:
-            logging.error(f"Erro ao enviar e-mail para {email_dest}: {e}")
+   mMsg = MIMEText(html, 'html', "utf-8")
+
+   mFrom = remetente
+   
+   mTo = dic['end_email']
+
+   mSubj = projeto +" - Aviso de tramitação em " + data_registro
+
+   mailhost.send(mMsg, mTo, mFrom, subject=mSubj)

5a834b07a6395c6e07022b1a29780ed94c25d686
OpenLegis <contato@openlegis.com.br>
2025-08-08 13:29:27 -0300

    Update legislaturas.py

--- openlegis/sagl/browser/legislaturas.py
+++ openlegis/sagl/browser/legislaturas.py
@@ -1,8 +1,7 @@
 # -*- coding: utf-8 -*-
 from five import grok
 from zope.publisher.interfaces import IPublishTraverse
-from zope.interface import implementer
-from zope.interface import Interface
+from zope.interface import implementer, Interface
 from io import BytesIO
 from Acquisition import aq_inner
 import requests
@@ -19,145 +18,130 @@ class Legislaturas(grok.View):
     grok.name('legislaturas')
 
     item_id = None
+    http = requests.Session()
 
     def publishTraverse(self, request, name):
-        request["TraversalRequestNameStack"] = []
-        ## Apenas se for um id numerico
-        if re.match(r"^\d+$", name):
-            self.item_id = name
+        request['TraversalRequestNameStack'] = []
+        if name.isdigit():
+            self.item_id = int(name)
         return self
+
+    def _build_service_url(self):
+        portal = self.context.portal_url.getPortalObject()
+        return f"{portal.absolute_url()}/@@legislaturas"
+
+    def render(self, num_legislatura=''):
+        self.service_url = self._build_service_url()
+        self.today = DateTime()
+
+        data = {
+            '@id': self.service_url,
+            '@type': 'Legislaturas',
+            'description': 'Lista de legislaturas',
+        }
+
+        if self.item_id is not None:
+            data.update(self.get_one(self.item_id))
+        elif num_legislatura:
+            data.update(self.get_one(int(num_legislatura)))
+        else:
+            data.update(self.lista())
+
+        return json.dumps(data, sort_keys=True, indent=2, ensure_ascii=False)
 
     def lista(self):
         portal = self.context.portal_url.getPortalObject()
-        portal_url = self.context.portal_url.portal_url()
-        service_url = self.portal_url + '/@@legislaturas'
-        lst_legislaturas = []
-        for item in self.context.zsql.legislatura_obter_zsql(ind_excluido=0):
-            dic_legislatura = {
-                "@id": self.service_url + '/' + str(item.num_legislatura),
-                "@type": 'Legislatura',
-                "id": str(item.num_legislatura),
-                "title": str(item.num_legislatura) + 'ª Legislatura',
-                "description": DateTime(item.dat_inicio_conv, datefmt='international').strftime("%d/%m/%Y") + ' a ' + DateTime(item.dat_fim_conv, datefmt='international').strftime("%d/%m/%Y"),
-            }
-            if (DateTime().strftime("%Y-%m-%d") > DateTime(item.dat_inicio_conv, datefmt='international').strftime("%Y-%m-%d") and DateTime().strftime("%Y-%m-%d") < DateTime(item.dat_fim_conv, datefmt='international').strftime("%Y-%m-%d")):
-               dic_legislatura['atual'] = True
-            else:
-               dic_legislatura['atual'] = False
-               
-            lst_legislaturas.append(dic_legislatura)
+        portal_url = portal.absolute_url()
+        service_url = self._build_service_url()
+        items = []
 
-        dic_legislaturas = {
-            "description": 'Lista de legislaturas ',
-            "items": lst_legislaturas,
+        for item in self.context.zsql.legislatura_obter_zsql(ind_excluido=0):
+            start = DateTime(item.dat_inicio_conv, datefmt='international')
+            end = DateTime(item.dat_fim_conv, datefmt='international')
+            atual = start <= self.today <= end
+
+            items.append({
+                '@id': f"{service_url}/{item.num_legislatura}",
+                '@type': 'Legislatura',
+                'id': str(item.num_legislatura),
+                'title': f"{item.num_legislatura}ª Legislatura",
+                'description': f"{start.strftime('%d/%m/%Y')} a {end.strftime('%d/%m/%Y')}",
+                'atual': bool(atual),
+            })
+
+        return {'description': 'Lista de legislaturas', 'items': items}
+
+    def get_one(self, num_legislatura):
+        portal = self.context.portal_url.getPortalObject()
+        portal_url = portal.absolute_url()
+        service_url = self._build_service_url()
+
+        res = list(self.context.zsql.legislatura_obter_zsql(num_legislatura=num_legislatura, ind_excluido=0))
+        if not res:
+            return {}
+
+        item = res[0]
+        start = DateTime(item.dat_inicio_conv, datefmt='international')
+        end = DateTime(item.dat_fim_conv, datefmt='international')
+
+        return {
+            '@id': f"{service_url}/{num_legislatura}",
+            '@type': 'Legislatura',
+            'id': str(num_legislatura),
+            'title': f"{num_legislatura}ª Legislatura",
+            'description': f"{start.strftime('%d/%m/%Y')} a {end.strftime('%d/%m/%Y')}",
+            'start': start.strftime('%Y-%m-%d'),
+            'end': end.strftime('%Y-%m-%d'),
+            'data_eleicao': DateTime(item.dat_eleicao_conv, datefmt='international').strftime('%Y-%m-%d'),
+            'items': self._get_vereadores(num_legislatura)
         }
-        
-        return dic_legislaturas
 
     def _get_vereadores(self, num_legislatura):
         portal = self.context.portal_url.getPortalObject()
-        portal_url = self.context.portal_url.portal_url()
-        service_url = self.portal_url + '/@@legislaturas'
-        lst_vereadores = []
+        portal_url = portal.absolute_url()
+        service_url = self._build_service_url()
+        vereadores = []
+
         for mandato in self.context.zsql.mandato_obter_zsql(num_legislatura=num_legislatura, ind_excluido=0):
-            parlamentar = self.context.zsql.parlamentar_obter_zsql(cod_parlamentar=mandato.cod_parlamentar, ind_excluido=0)[0]
-            dic_vereador = {
-               "@id":  portal_url + '/@@vereadores/' + parlamentar.cod_parlamentar,
-               "@type": 'Vereador',
-               "id": parlamentar.cod_parlamentar,
-               "title": parlamentar.nom_parlamentar,
-               "description": parlamentar.nom_completo,
-            }
-
-            dic_vereador['votos'] = ''  
-            if mandato.num_votos_recebidos != None:
-               dic_vereador['votos'] = mandato.num_votos_recebidos
-            
-            if mandato.ind_titular == 1:
-               dic_vereador['mandato'] = 'Titular'
+            par = self.context.zsql.parlamentar_obter_zsql(cod_parlamentar=mandato.cod_parlamentar, ind_excluido=0)[0]
+            foto_field = f"{mandato.cod_parlamentar}_foto_parlamentar"
+            if hasattr(self.context.sapl_documentos.parlamentar.fotos, foto_field):
+                url_foto = f"{portal_url}/sapl_documentos/parlamentar/fotos/{foto_field}"
+                try:
+                    content = self.http.get(url_foto).content
+                    img = Image.open(BytesIO(content))
+                    image_info = [{
+                        'content-type': f"image/{img.format.lower()}",
+                        'download': url_foto,
+                        'filename': foto_field,
+                        'width': img.width,
+                        'height': img.height,
+                        'size': len(content)
+                    }]
+                except UnidentifiedImageError:
+                    image_info = []
             else:
-               dic_vereador['mandato'] = 'Suplente'
+                url_foto = f"{portal_url}/imagens/avatar.png"
+                image_info = []
 
-            dic_vereador['cod_autor'] = ''
-            for autor in self.context.zsql.autor_obter_zsql(cod_parlamentar=parlamentar.cod_parlamentar):
-                dic_vereador['cod_autor'] = autor.cod_autor
+            partidos = []
+            for fil in self.context.zsql.parlamentar_data_filiacao_obter_zsql(num_legislatura=num_legislatura, cod_parlamentar=mandato.cod_parlamentar):
+                if fil.dat_filiacao:
+                    for pt in self.context.zsql.parlamentar_partido_obter_zsql(dat_filiacao=fil.dat_filiacao, cod_parlamentar=mandato.cod_parlamentar):
+                        partidos.append({'token': pt.sgl_partido, 'title': pt.nom_partido})
 
-            lst_imagem = []
-            foto = str(mandato.cod_parlamentar) + "_foto_parlamentar"
-            if hasattr(self.context.sapl_documentos.parlamentar.fotos, foto):
-               url = portal_url + '/sapl_documentos/parlamentar/fotos/' + foto
-               dic_vereador['url_foto'] = url
-               response = requests.get(url)
-               try:
-                  img = Image.open(BytesIO(response.content))
-                  dic_image = {
-                    "content-type": 'image/' + str(img.format).lower(),
-                    "download": url,
-                    "filename": foto,
-                    "width": str(img.width),
-                    "height": str(img.height),
-                    "size": str(len(img.fp.read()))
-                  }
-               except UnidentifiedImageError:
-                  dic_image = {}
-               lst_imagem.append(dic_image)
-            else:
-               dic_vereador['url_foto'] = self.portal_url + '/imagens/avatar.png' 
-            dic_vereador['image'] = lst_imagem 
-                    
-            lst_partido = []
-            for filiacao in self.context.zsql.parlamentar_data_filiacao_obter_zsql(num_legislatura=num_legislatura, cod_parlamentar=mandato.cod_parlamentar):    
-                if filiacao.dat_filiacao != '0' and filiacao.dat_filiacao != None:
-                   for partido in self.context.zsql.parlamentar_partido_obter_zsql(dat_filiacao=filiacao.dat_filiacao, cod_parlamentar=mandato.cod_parlamentar):
-                      dic_partido = {
-                         "token": partido.sgl_partido,
-                         "title": partido.nom_partido
-                      }
-                   lst_partido.append(dic_partido)
-            dic_vereador['partido'] = lst_partido
-            lst_vereadores.append(dic_vereador)            
-            lst_vereadores.sort(key=lambda dic_vereador: dic_vereador['description'])
-        return lst_vereadores
+            vereadores.append({
+                '@id': f"{portal_url}/@@vereadores/{par.cod_parlamentar}",
+                '@type': 'Vereador',
+                'id': par.cod_parlamentar,
+                'title': par.nom_parlamentar,
+                'description': par.nom_completo,
+                'votos': mandato.num_votos_recebidos or 0,
+                'mandato': 'Titular' if mandato.ind_titular else 'Suplente',
+                'cod_autor': (self.context.zsql.autor_obter_zsql(cod_parlamentar=par.cod_parlamentar) or [{}])[0].get('cod_autor', ''),
+                'url_foto': url_foto,
+                'image': image_info,
+                'partido': partidos
+            })
 
-    def get_one(self, item_id):
-        item_id = int(item_id)
-        results = [item for item in self.context.zsql.legislatura_obter_zsql(num_legislatura=item_id, ind_excluido=0)]
-        if not results:
-            return {}
-        item = results[0]
-        num_legislatura = str(item.num_legislatura)
-        dic_legislatura = {
-            "@id": self.service_url + '/' + num_legislatura,
-            "@type": 'Legislatura',
-            "id": num_legislatura,
-            "title": str(item.num_legislatura) + 'ª Legislatura' ,
-            "description": DateTime(item.dat_inicio_conv, datefmt='international').strftime("%d/%m/%Y") + ' a ' + DateTime(item.dat_fim_conv, datefmt='international').strftime("%d/%m/%Y"),
-            "start": DateTime(item.dat_inicio_conv, datefmt='international').strftime("%Y-%m-%d"),
-            "end": DateTime(item.dat_fim_conv, datefmt='international').strftime("%Y-%m-%d"),
-            "data_eleicao": DateTime(item.dat_eleicao_conv, datefmt='international').strftime("%Y-%m-%d")
-        }
-        # Vereadores
-        dic_legislatura['items'] = self._get_vereadores(item.num_legislatura)
-
-        return dic_legislatura
-
-    def render(self, num_legislatura=''):
-        self.portal = self.context.portal_url.getPortalObject()
-        self.portal_url = self.context.portal_url.portal_url()
-        self.service_url = self.portal_url + '/@@legislaturas'
-        self.hoje = DateTime()
-        data = {
-           '@id':  self.service_url,
-           '@type':  'Legislaturas',
-           'description':  'Lista de legislaturas',
-        }
-        if self.item_id:
-            data.update(self.get_one(self.item_id))
-        elif num_legislatura != '':
-            data.update(self.lista(num_legislatura))
-        else:
-            data.update(self.lista())
-            
-        serialized = json.dumps(data, sort_keys=True, indent=2, ensure_ascii=False).encode('utf8')
-        return (serialized.decode())
-
+        return sorted(vereadores, key=lambda x: x['description'])
